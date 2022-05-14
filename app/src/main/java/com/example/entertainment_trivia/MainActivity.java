2200187d31999e411a863a53435e9f63efff08b3
Nate <n.teku710@gmail.com>
2022-05-13 17:54:00 -0700

    Start up Activity Implemented

--- app/src/main/java/com/example/entertainment_trivia/MainActivity.java
+++ app/src/main/java/com/example/entertainment_trivia/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.entertainment_trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener((v) -> {
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
