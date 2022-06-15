da672a3c4c2ca9a468f83e2374065990e54849db
nteku <nteku@email.arizona.edu>
2022-06-14 20:46:49 -0700

    Cleaned up MovieActivity Activity and updated trivia result View

--- app/src/main/java/com/example/entertainment_trivia/MovieActivity.java
+++ app/src/main/java/com/example/entertainment_trivia/MovieActivity.java
@@ -30,6 +30,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -51,17 +52,17 @@ public class MovieActivity extends AppCompatActivity {
     private Button option3;
     private Button nextButton;
     private String answer;
-    private int questionsCorrect;
-    private int questionsDone = 0;
+    private double questionsCorrect;
+    private double questionsDone = 0;
     private  List <List<String>> log;
     private List <Integer> images;
     private ImageView currentImage;
-    private int totalQuestions;
+    private double totalQuestions;
     private HashMap <Integer,List <String>> info;
     private List <String> currentLog;
-    final private int height =  48;
-    final private int width = 422;
-    //private LinearLayout.LayoutParams params;
+    private double correctPercentage;
+
+
 
 
     @Override
@@ -87,35 +88,28 @@ public class MovieActivity extends AppCompatActivity {
         nextButton = findViewById(R.id.nextButton);
         currentImage = findViewById(R.id.image);
 
-      //  LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)option1.getLayoutParams();
-
-        currentImage.setImageResource( images.get(questionsDone));
-        question.setText(info.get(images.get(questionsDone)).get(0));
-        option1.setText(info.get(images.get(questionsDone)).get(1));
-        option2.setText(info.get(images.get(questionsDone)).get(2));
-        option3.setText(info.get(images.get(questionsDone)).get(3));
-        answer = info.get(images.get(questionsDone)).get(4);
 
 
-        /*
-        question.setText(log.get(questionsDone).get(0));
-        option1.setText( log.get(questionsDone).get(1));
-        option2.setText(log.get(questionsDone).get(2));
-        option3.setText(log.get(questionsDone).get(3));
-        answer = log.get(questionsDone).get(4);
-        */
+        currentImage.setImageResource( images.get((int) questionsDone));
+        question.setText(info.get(images.get((int) questionsDone)).get(0));
+        option1.setText(info.get(images.get((int) questionsDone)).get(1));
+        option2.setText(info.get(images.get((int) questionsDone)).get(2));
+        option3.setText(info.get(images.get((int) questionsDone)).get(3));
+        answer = info.get(images.get( (int) questionsDone)).get(4);
+
+
 
 
         option1.setOnClickListener((v) -> {
             if (option1.getText().equals(answer) ) {
-                option1.setBackgroundColor(Color.parseColor("#00FF00"));
+                option1.setBackground(getResources().getDrawable(R.drawable.correct));
                 option1.setText("Correct");
                 questionsCorrect++;
                 questionsDone++;
                 disableButtons();
             }
             else{
-                option1.setBackgroundColor(Color.parseColor("#FF0000"));
+                option1.setBackground(getResources().getDrawable(R.drawable.incorrect));
                 option1.setText("Incorrect");
                 questionsDone++;
                 disableButtons();
@@ -127,14 +121,14 @@ public class MovieActivity extends AppCompatActivity {
 
         option2.setOnClickListener((v) -> {
             if (option2.getText().equals(answer) ) {
-                option2.setBackgroundColor(Color.parseColor("#00FF00"));
+                option2.setBackground(getResources().getDrawable(R.drawable.correct));
                 option2.setText("Correct");
                 questionsCorrect++;
                 questionsDone++;
                 disableButtons();
             }
             else{
-                option2.setBackgroundColor(Color.parseColor("#FF0000"));
+                option2.setBackground(getResources().getDrawable(R.drawable.incorrect));
                 option2.setText("Incorrect");
                 questionsDone++;
                 disableButtons();
@@ -146,18 +140,15 @@ public class MovieActivity extends AppCompatActivity {
        // TODO : Implement the event handling correct and incorrect xml files for these buttons
         option3.setOnClickListener((v) -> {
             if (option3.getText().equals(answer) ) {
-                option3.setBackgroundColor(Color.parseColor("#00FF00"));
+                option3.setBackground(getResources().getDrawable(R.drawable.correct));
                 option3.setText("Correct");
-                option3.setTextColor(Color.green(5));
                 questionsCorrect++;
                 questionsDone++;
                 disableButtons();
             }
             else{
-                //option3.setBackgroundColor(Color.parseColor("#FF0000"));
                 option3.setText("Incorrect");
                 option3.setBackground(getResources().getDrawable(R.drawable.incorrect));
-               // option3.setTextColor(Color.parseColor("#FF0000"));
                 questionsDone++;
                 disableButtons();
 
@@ -187,17 +178,7 @@ public class MovieActivity extends AppCompatActivity {
     }
     public void initializeLists(){
 
-
-
-         List<String> question1 = new ArrayList<>();
-         List<String> question2 = new ArrayList<>();
-         List <String> question3 = new ArrayList<>();
-
-         currentLog = new ArrayList<>();
-
-
-
-
+        currentLog = new ArrayList<>();
 
 
         try {
@@ -222,35 +203,6 @@ public class MovieActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-
-
-         /*
-         question1.add("Which movie was Denzel Washington not in?");
-         question1.add("Philadelphia");
-         question1.add("Malcolm X");
-         question1.add("Ali");
-         question1.add("Ali");
-
-
-        question2.add("What is the highest grossed movie franchise of all time?");
-        question2.add("Star Wars");
-        question2.add("Marvel Cinematic Universe");
-        question2.add("Harry Potter");
-        question2.add( "Marvel Cinematic Universe");
-
-
-        question3.add("Based on this image, what movie did this cast star in?");
-        question3.add("Scooby-Doo");
-        question3.add("Mamma Mia!");
-        question3.add("2 Hearts");
-        question3.add("Scooby-Doo");
-
-
-        log.add(question1);
-        log.add(question2);
-        log.add(question3);
-
-         */
         images.add(R.drawable.denzelwashington);
         images.add( R.drawable.money);
         images.add(R.drawable.scoobydoo);
@@ -261,8 +213,7 @@ public class MovieActivity extends AppCompatActivity {
         }
         Collections.shuffle(images);
 
-        // Create A HashMap that stores an integer and a list, which then have a list of integers which shuffles the order of it
-        // It crashed so now do a log.debug session to see what is the issue
+
     }
 
     public void gameResult(){
@@ -274,13 +225,27 @@ public class MovieActivity extends AppCompatActivity {
                  option2.setVisibility(View.GONE);
                  option3.setVisibility(View.GONE);
                  currentImage.setVisibility(View.GONE);
-                 if (  (double) (questionsCorrect/totalQuestions) > 0.5){
-                     question.setVisibility(View.GONE);
-                     heading.setText("You got " + questionsCorrect + " out of " + totalQuestions + " correct. Good job!");
+                 question.setVisibility(View.GONE);
 
+
+                 correctPercentage = (double) (questionsCorrect/totalQuestions) * 100;
+
+
+
+                 if (   (correctPercentage  >= 0) && (correctPercentage <= 59)){
+                     heading.setText("You got " + (int) questionsCorrect + " out of " + (int) totalQuestions + " correct. Try again.");
+                 }
+                 else if ( (correctPercentage >= 60) && (correctPercentage <= 69)){
+                     heading.setText("You got " + (int) questionsCorrect + " out of " + (int) totalQuestions + " correct. Not looking so good.");
+                 }
+                 else if ( (correctPercentage >= 70) && (correctPercentage <= 79)){
+                     heading.setText("You got " + (int) questionsCorrect + " out of " + (int) totalQuestions + " correct. Could do better.");
+                 }
+                 else if ( (correctPercentage >= 80)  && (correctPercentage <= 89)){
+                     heading.setText("You got " + (int) questionsCorrect + " out of " + (int) totalQuestions + " correct. Great job!");
                  }
                  else{
-                     question.setText("You got " + questionsCorrect + " out of " + totalQuestions + " correct. Better luck next time.");
+                     heading.setText("You got " + (int) questionsCorrect + " out of " + (int) totalQuestions +  " correct. Excellent work!");
                  }
 
 
@@ -298,45 +263,20 @@ public class MovieActivity extends AppCompatActivity {
     public void setBackToDefault( ){
 
 
-        currentImage.setImageResource(images.get(questionsDone));
-        question.setText(info.get(images.get(questionsDone)).get(0));
-        option1.setText( info.get(images.get(questionsDone)).get(1));
-        option2.setText(info.get(images.get(questionsDone)).get(2));
-        option3.setText(info.get(images.get(questionsDone)).get(3));
-        /*
-        question.setText(log.get(questionsDone).get(0));
-        option1.setText( log.get(questionsDone).get(1));
-        option2.setText(log.get(questionsDone).get(2));
-        option3.setText(log.get(questionsDone).get(3));
+        currentImage.setImageResource(images.get((int) questionsDone));
+        question.setText(info.get(images.get((int) questionsDone)).get(0));
+        option1.setText( info.get(images.get((int) questionsDone)).get(1));
+        option2.setText(info.get(images.get((int) questionsDone)).get(2));
+        option3.setText(info.get(images.get((int) questionsDone)).get(3));
 
-         */
-        int dpHeight = (int) (height/ Resources.getSystem().getDisplayMetrics().density);
-        int dpWidth = (int) (width/ Resources.getSystem().getDisplayMetrics().density);
-       // LinearLayout.LayoutParams params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.dpWidth,dpHeight);
         option1.setBackground(getResources().getDrawable(R.drawable.gradient));
-        /*
-        option1.setBackgroundColor(R.drawable.gradient);
-        /*
-        option1.setLayoutParams();
-        option1.setWidth(422);
-        option1.setHeight(48);
-
-
-
-        option2.setBackgroundColor(R.drawable.gradient);
-
-
-
-        option3.setBackgroundColor(R.drawable.gradient);
-
-   */
-
+        option2.setBackground(getResources().getDrawable(R.drawable.gradient));
+        option3.setBackground(getResources().getDrawable(R.drawable.gradient));
 
         option1.setEnabled(true);
         option2.setEnabled(true);
         option3.setEnabled(true);
-        //answer = log.get(questionsDone).get(4);
-        answer = info.get(images.get(questionsDone)).get(4);
+        answer = info.get(images.get((int) questionsDone)).get(4);
 
     }
 
