1e0859a4a611d870058cfb2771308643f247e072
nteku <nteku@email.arizona.edu>
2022-06-26 21:27:10 -0700

    Redirected TextView in Main to prompt User to register

--- app/src/main/java/com/example/entertainment_trivia/LoginActivity.java
+++ app/src/main/java/com/example/entertainment_trivia/LoginActivity.java
@@ -1,14 +1,42 @@
 package com.example.entertainment_trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // TODO Implement the event handling when signing up the user
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+        //animationBackground();
+
     }
+
+
+    public void animationBackground(){
+        ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+
+    }
+
+
+
+
+
 }
