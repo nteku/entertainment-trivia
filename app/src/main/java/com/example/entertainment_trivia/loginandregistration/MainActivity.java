ee0cf8509f850d65a29cb37acf255dd3a211a8f2
nteku <nteku@email.arizona.edu>
2022-08-03 22:39:50 -0700

    Commented Main and Register files

--- app/src/main/java/com/example/entertainment_trivia/loginandregistration/MainActivity.java
+++ app/src/main/java/com/example/entertainment_trivia/loginandregistration/MainActivity.java
@@ -5,14 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.entertainment_trivia.R;
 import com.example.entertainment_trivia.account.Account;
 import com.example.entertainment_trivia.menu.MenuActivity;
@@ -24,81 +21,117 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button startButton;
-    private TextView loggingIn;
-    private  EditText email;
-    private  EditText password;
+/**
+ * Class:   MainActivity.java
+ * Purpose: This activity is meant as a login screen for a user
+ * @author  Nathan Teku
+ */
+public class MainActivity extends AppCompatActivity
+{
+    // private attributes needed for this class
+    private Button loginButton;
+    private TextView registeringLink;
+    private EditText email;
+    private EditText password;
     private Account account;
     private ProgressBar progressBar;
-
-
+    private FirebaseAuth fAuth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // hiding action bar
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        // creating view
         setContentView(R.layout.activity_main);
 
-        startButton = findViewById(R.id.startButton);
-        loggingIn = findViewById(R.id.logInLink);
+        // catching the ID's of the private attributes from the xml file
+        loginButton = findViewById(R.id.loginButton);
+        registeringLink = findViewById(R.id.registrationLink);
         email = findViewById(R.id.editTextTextEmailAddress2);
         password = findViewById(R.id.editTextTextPassword);
         progressBar = findViewById(R.id.progressBar);
 
-
+        // setting progress bar invisible
         progressBar.setVisibility(View.INVISIBLE);
 
-        loggingIn.setOnClickListener( (v) -> {
+        // capturing the authentication of Firebase
+        fAuth = FirebaseAuth.getInstance();
+
+        // if user wants register a new account
+        registeringLink.setOnClickListener( (v) ->
+        {
             progressBar.setVisibility(View.VISIBLE);
-            Intent intent = new Intent(this, LoginActivity.class);
+
+            // going to LoginActivity link
+            Intent intent = new Intent(this, RegistrationActivity.class);
             startActivity(intent);
         });
 
-        if (FirebaseAuth.getInstance().getCurrentUser() != null){
-
-            FirebaseDatabase.getInstance().getReference().child(getResources().getString(R.string.users)).child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+        // if the user is already logged in
+        if (fAuth.getCurrentUser() != null)
+        {
+            // retrieving the object from Firebase
+            FirebaseDatabase.getInstance().getReference().child(getResources().getString(R.string.users)).child(fAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener()
+            {
                 @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                public void onDataChange(@NonNull DataSnapshot snapshot)
+                {
+                    // saving the object and going to the MenuActivity with account being passed through
                     account = (snapshot.getValue(Account.class));
                     Intent intent = new Intent(MainActivity.this, MenuActivity.class);
                     intent.putExtra(getResources().getString(R.string.account), account);
                     startActivity(intent);
                 }
                 @Override
-                public void onCancelled(@NonNull DatabaseError error) {
+                public void onCancelled(@NonNull DatabaseError error)
+                {
                     Toast.makeText(MainActivity.this,getResources().getString(R.string.successful_Message),Toast.LENGTH_SHORT).show();
                 }
             });
         }
 
-        startButton.setOnClickListener(new View.OnClickListener() {
+        // if the button to log in is clicked
+        loginButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-
-                if (!emptyCredential()) {
-                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            public void onClick(View v)
+            {
+                // if there are any credentials that are empty
+                if (!emptyCredential())
+                {
+                    // authenticate the email and password being inputted to Firebase
+                    fAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                    {
                         @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if (task.isSuccessful())
+                            {
+
+                                // set the visible progress bar to visible
                                 progressBar.setVisibility(View.VISIBLE);
-                                FirebaseDatabase.getInstance().getReference().child(getResources().getString(R.string.users)).child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+
+                                // retrieve the object from Firebase Realtime Database
+                                FirebaseDatabase.getInstance().getReference().child(getResources().getString(R.string.users)).child(fAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener()
+                                {
                                     @Override
-                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    public void onDataChange(@NonNull DataSnapshot snapshot)
+                                    {
+                                        // saving the object and going to the MenuActivity with account being passed through
                                         account = (snapshot.getValue(Account.class));
                                         Intent intent = new Intent(v.getContext(), MenuActivity.class);
                                         intent.putExtra(getResources().getString(R.string.account), account);
                                         startActivity(intent);
                                     }
                                     @Override
-                                    public void onCancelled(@NonNull DatabaseError error) {
-                                        Toast.makeText(MainActivity.this,getResources().getString(R.string.login_failed),Toast.LENGTH_SHORT).show();
+                                    public void onCancelled(@NonNull DatabaseError error)
+                                    {
                                     }
                                 });
                             }
@@ -112,23 +145,37 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-        public boolean emptyCredential(){
-            if (email.getText().toString().isEmpty() && password.getText().toString().isEmpty()){
+    /**
+     * Method:  emptyCredential()
+     * Purpose: Detects if either the password or email slots are left empty when logging in
+     * @return
+     */
+    public boolean emptyCredential()
+    {
+        // if both the email and password slots are empty, trigger an error
+        if (email.getText().toString().isEmpty() && password.getText().toString().isEmpty())
+        {
+              email.setError(getResources().getString(R.string.empty_Email));
+              password.setError(getResources().getString(R.string.empty_Password));
+              return true;
+        }
+        else
+        {
+            // if only the email is left empty, trigger an error
+            if (email.getText().toString().isEmpty())
+            {
                 email.setError(getResources().getString(R.string.empty_Email));
+                return true;
+            }
+            // if only the password is left empty, trigger an error
+            if (password.getText().toString().isEmpty())
+            {
                 password.setError(getResources().getString(R.string.empty_Password));
                 return true;
             }
-            else{
-                if (email.getText().toString().isEmpty()){
-                    email.setError(getResources().getString(R.string.empty_Email));
-                    return true;
-                }
-                if (password.getText().toString().isEmpty()){
-                    password.setError(getResources().getString(R.string.empty_Password));
-                    return true;
-                }
 
-            }
-                return false;
         }
+        // if nothing is empty
+        return false;
+    }
 }
---
+++ app/src/main/java/com/example/entertainment_trivia/loginandregistration/RegistrationActivity.java
@@ -0,0 +1,155 @@
+package com.example.entertainment_trivia.loginandregistration;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.example.entertainment_trivia.R;
+import com.example.entertainment_trivia.account.Account;
+import com.example.entertainment_trivia.menu.MenuActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import java.util.Objects;
+
+/**
+ * Class:   RegistrationActivity.java
+ * Purpose: Registers a user to the database
+ * @author  Nathan Teku
+ */
+public class RegistrationActivity extends AppCompatActivity
+{
+    // private attributes needed for this class
+    private Button signUpButton;
+    private EditText name;
+    private EditText email;
+    private EditText password;
+    private FirebaseAuth fAuth;
+    private ProgressBar progressBar;
+    private Account account;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // hiding action bar
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        // creating view
+        setContentView(R.layout.activity_register);
+
+        // catching the ID's of the private attributes from the xml file
+        signUpButton = findViewById(R.id.signUp);
+        name = findViewById(R.id.editTextTextPersonName);
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+        progressBar = findViewById(R.id.progressBar);
+
+        // setting progress bar to invisible
+        progressBar.setVisibility(View.INVISIBLE);
+
+        // capturing the authentication of Firebase
+        fAuth = FirebaseAuth.getInstance();
+
+        // if the button to log in is clicked
+        signUpButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (!emptyCredential())
+                {
+                    // creating a account on user based on email and password through Firebase
+                    fAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                            .addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>()
+                            {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task)
+                                {
+                                    if (task.isSuccessful())
+                                    {
+                                        // set the visible progress bar to visible
+                                        progressBar.setVisibility(View.VISIBLE);
+
+                                        // creating an account based on the info of the user
+                                        account = new Account(name.getText().toString(), email.getText().toString(), password.getText().toString(), "0");
+
+                                        // placing account object to Firebase Realtime Database
+                                        FirebaseDatabase.getInstance().getReference(getResources().getString(R.string.users))
+                                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(account).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task)
+                                                    {
+                                                        if (task.isSuccessful())
+                                                        {
+                                                            // create toast message and going to the MenuActivity with account being passed through
+                                                            Toast.makeText(RegistrationActivity.this, getResources().getString(R.string.successful_register),Toast.LENGTH_SHORT).show();
+                                                            Intent intent = new Intent(v.getContext(), MenuActivity.class);
+                                                            intent.putExtra(getResources().getString(R.string.account), account);
+                                                            startActivity(intent);
+                                                        }
+                                                        else
+                                                        {
+                                                            Toast.makeText(RegistrationActivity.this, getResources().getString(R.string.failed_register), Toast.LENGTH_SHORT).show();
+                                                        }
+                                                    }
+                                                });
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    /**
+     * Method:  emptyCredential()
+     * Purpose: Detects if either the password or email slots are left empty when logging in
+     * @return
+     */
+    public boolean emptyCredential()
+    {
+        // if both the email and password slots are empty, trigger an error
+        if ( name.getText().toString().isEmpty() && email.getText().toString().isEmpty() && password.getText().toString().isEmpty()){
+            name.setError(getResources().getString(R.string.empty_Name));
+            email.setError(getResources().getString(R.string.empty_Email));
+            password.setError(getResources().getString(R.string.empty_Password));
+            return true;
+        }
+        else
+        {
+            // if only the name is left empty, trigger an error
+            if (name.getText().toString().isEmpty())
+            {
+                name.setError(getResources().getString(R.string.empty_Name));
+                return true;
+            }
+            // if only the email is left empty, trigger an error
+            if (email.getText().toString().isEmpty())
+            {
+                email.setError(getResources().getString(R.string.empty_Email));
+                return true;
+            }
+            // if only the password is left empty, trigger an error
+            if (password.getText().toString().isEmpty())
+            {
+                password.setError(getResources().getString(R.string.empty_Password));
+                return true;
+            }
+        }
+        // if nothing is empty
+        return false;
+    }
+
+}
